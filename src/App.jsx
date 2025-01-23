ea9e5d00189f2628398a9f618dcc335db6848a4a
Augusto <villevip10@gmail.com>
2025-01-23 10:58:53 -0300

    Explorar act

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ export const App = () => {
 
       {/* EXPLORAR */}
       <Element name="Productos">
-        <section className="py-3 md:pt-12 bg-gray-100">
+        <section className="py-3 pt-10 bg-gray-100">
           <div className="container mx-auto py-8 px-4 flex flex-col justify-center">
             <div className="grid grid-cols-4 grid-rows-3 md:grid-cols-3 md:grid-rows-3 gap-4">
               {/* Auriculares */}
