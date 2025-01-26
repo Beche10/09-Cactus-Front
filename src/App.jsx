8ba50dc9c4cf0362c60bbc6e22b2c175c05f1d39
Augusto <villevip10@gmail.com>
2025-01-26 18:45:29 -0300

    Footerpy

--- src/App.jsx
+++ src/App.jsx
@@ -180,7 +180,7 @@ export const App = () => {
           </div>
 
           {/* Secciones */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-20 items-center px-4 md:px-8 md:py-10">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-20 items-center px-4 md:px-8 py-6 md:py-10">
             {/* Logo */}
             <div className="flex flex-col items-center justify-center -space-y-2">
               <LazyLoadImage
@@ -283,7 +283,7 @@ export const App = () => {
           </div>
 
           {/* Divider */}
-          <div className="border-t border-gray-800 my-6"></div>
+          <div className="border-t border-gray-800 my-5"></div>
 
           <div className="flex flex-col items-center text-center text-sm text-gray-400 px-4 space-y-4">
             {/* Enlaces Rápidos */}
